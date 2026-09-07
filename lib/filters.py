821da2b5d45ff4cd03f6d7c01ce1356a4bbbbdd3
Brad Arnold <brad1379@gmail.com>
2026-09-07 16:49:43 -0500

    implemented all functions

--- lib/filters.py
+++ lib/filters.py
@@ -7,4 +7,6 @@ def filter_students_by_major(student_list, major):
     - Check if a student's major matches the given major (case insensitive).
     - Return a new list containing only students that match.
     """
-    pass
+    filtered_students = [student for student in student_list if student[2].lower() == major.lower()]
+
+    return filtered_students
